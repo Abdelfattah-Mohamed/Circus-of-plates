9c79ef048eff440132857d5d20034a0c39dd8e6c
Your Name <you@example.com>
2018-12-22 23:14:26 +0200

    facad

--- src/world_class/Circus.java
+++ src/world_class/Circus.java
@@ -3,25 +3,11 @@ package world_class;
 import momento.*;
 import eg.edu.alexu.csd.oop.game.GameObject;
 import eg.edu.alexu.csd.oop.game.World;
-import observer.*;
 import observer.Observer;
 import strategy_difficulty.Strategy;
 import dynamicLinkage.jarread;
-import iterator.Iterator;
-import iterator.Iterator_concrete;
-import to_come.ConstantImageObject;
-import to_come.ControlledImageObject;
-import to_come.FlyWeight;
-import to_come.IShape;
-import to_come.ImageObject;
-import to_come.MovingImageObject;
-import to_come.MovingPool;
-import to_come.State;
+import to_come.*;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
-import java.awt.image.BufferedImage;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 
@@ -33,10 +19,6 @@ public class Circus implements World {
 	private final int width;
 	private final int height;
 	private Strategy difficulty;
-	private Observer t;
-	private Observer s;
-	private Observer sc;
-	private Observer mom;
 	private boolean flag = false;
 	private ArrayList<GameObject> constant = new ArrayList<GameObject>();
 	private ArrayList<GameObject> moving = new ArrayList<GameObject>();
@@ -48,13 +30,76 @@ public class Circus implements World {
 	private static Originator originator = new Originator();
 	private int currentMementoL = 0;
 	private int currentMementoR = 0;
-	// private int countL = 0;
-	// private int countR = 0;
 	private ImageObject dummyL;
 	private ImageObject dummyR;
-	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	double scrwidth = screenSize.getWidth();
-	double scrheight = screenSize.getHeight();
+	private Facad logic;
+
+
+	public List<Observer> getObservers() {
+		return observers;
+	}
+
+	public ArrayList<GameObject> getMoving() {
+		return moving;
+	}
+
+	public ArrayList<GameObject> getControl() {
+		return control;
+	}
+
+	public static int getMaxTime() {
+		return MAX_TIME;
+	}
+
+	public long getStartTime() {
+		return startTime;
+	}
+
+	public void setControlL(ArrayList<GameObject> controlL) {
+		this.controlL = controlL;
+	}
+
+	public void setControlR(ArrayList<GameObject> controlR) {
+		this.controlR = controlR;
+	}
+
+	public ArrayList<GameObject> getControlL() {
+		return controlL;
+	}
+
+	public ArrayList<GameObject> getControlR() {
+		return controlR;
+	}
+
+	public Strategy getDifficulty() {
+		return difficulty;
+	}
+
+	public int getCurrentMementoL() {
+		return currentMementoL;
+	}
+
+	public int getCurrentMementoR() {
+		return currentMementoR;
+	}
+
+	public static Caretaker getCaretaker() {
+		return caretaker;
+	}
+
+	public void setCurrentMementoL(int currentMementoL) {
+		this.currentMementoL = currentMementoL;
+	}
+
+	public void setCurrentMementoR(int currentMementoR) {
+		this.currentMementoR = currentMementoR;
+	}
+
+	public static Originator getOriginator() {
+		return originator;
+	}
+
+
 
 	public Circus(int width, int height, Strategy difficulty) {
 		this.width = width;
@@ -65,7 +110,6 @@ public class Circus implements World {
 		List<Class<?>> listofClasses = x.getCrunchifyClassNamesFromJar("JAR_F.jar");
 		IShape shape = null;
 		state = new ConstantImageObject();
-
 		try {
 			shape = (IShape) listofClasses.get(3).getConstructor(String.class).newInstance("0");
 		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
@@ -100,18 +144,16 @@ public class Circus implements World {
 		control.add(clown);
 		control.add(dummyL);
 		control.add(dummyR);
-
 		controlR.add(dummyR);
 		controlL.add(dummyL);
 		// TODO first state in memento
-		saveStateL();
-		saveStateR();
 		// play some music
-		//addition_classes.Sound.getInstance().startCircusSound();
+		addition_classes.Sound.getInstance().startCircusSound();
 		// moving objects (plates)
 		MovingPool mpl = MovingPool.getInstance();
 		mpl.setPool(width, height);
 		moving = mpl.usePool();
+		logic=new Facad(this);
 	}
 
 	@Override
@@ -129,28 +171,7 @@ public class Circus implements World {
 		return control;
 	}
 
-	public void setControlL(ArrayList<GameObject> controlL) {
-		this.controlL = controlL;
-	}
-
-	public void setControlR(ArrayList<GameObject> controlR) {
-		this.controlR = controlR;
-	}
-
-	public ArrayList<GameObject> getControlL() {
-		return controlL;
-	}
-
-	public ArrayList<GameObject> getControlR() {
-		return controlR;
-	}
-
-	public Strategy getDifficulty() {
-		return difficulty;
-	}
-
 	@Override
-
 	public int getWidth() {
 		return width;
 	}
@@ -160,192 +181,22 @@ public class Circus implements World {
 		return height;
 	}
 
-	public static void setCaretaker(Caretaker caretaker) {
-		Circus.caretaker = caretaker;
+
+	public ImageObject getDummyL() {
+		return dummyL;
 	}
 
-	public int getCurrentMementoL() {
-		return currentMementoL;
-	}
-
-	public int getCurrentMementoR() {
-		return currentMementoR;
-	}
-
-	public static void setOriginator(Originator originator) {
-		Circus.originator = originator;
-
-	}
-
-	public static Caretaker getCaretaker() {
-		return caretaker;
-	}
-
-	public void setCurrentMementoL(int currentMementoL) {
-		this.currentMementoL = currentMementoL;
-	}
-
-	public void setCurrentMementoR(int currentMementoR) {
-		this.currentMementoR = currentMementoR;
-	}
-
-	public static Originator getOriginator() {
-		return originator;
+	public ImageObject getDummyR() {
+		return dummyR;
 	}
 
 	@Override
 	public boolean refresh() {
-		System.out.println(moving.size());
-		if (!flag) {
-			t = new Time(this);
-			s = new Sound(this);
-			sc = new Score(this);
-			mom = new plateServer(this);
-			flag = true;
-		}
-		MovingPool mpl = MovingPool.getInstance();
-		if(mpl.hasElement()) {
-			State state = new MovingImageObject();
-			GameObject obj = mpl.useObj();
-			((ImageObject)obj).setState(state);
-			obj.setX((int) (Math.random() * width));
-			obj.setY((int) (Math.random() * height));
-			moving.add(obj);
-		}
-		
-		boolean timeout = System.currentTimeMillis() - startTime > MAX_TIME; // time end and game over
-
-		// System.out.print(System.currentTimeMillis() +" " );
-		// System.out.print(startTime+" ");
-		// System.out.println(MAX_TIME);
-
-		// last plate in both stacks0
-		GameObject lastplateL = controlL.get(controlL.size() - 1);
-		GameObject lastplateR = controlR.get(controlR.size() - 1);
-		Iterator_concrete xx;
-		xx = new Iterator_concrete(moving); // handle moving plates here
-
-		for (Iterator iter = xx.getIterator(0); iter.hasNext();) {
-			ImageObject o = (ImageObject) iter.next();
-			o.setY((o.getY() + 1));
-			if (o.getY() + 100 >= getHeight()) {
-				// reuse the plate in another position
-				o.setY(-1 * (int) (Math.random() * getHeight()));
-				o.setX((int) (Math.random() * getWidth()));
-				// State state = new MovingImageObject();
-				// moving.add(new ImageObject((int) (Math.random() * width), (int)
-				// (Math.random() * height / 2), "/plate.png",
-				// state));
-			}
-			// o.setX(o.getX() + (Math.random() > 0.5 ? 2 : -2));
-			// al taba2 b3dha l taba2 ele fo2
-			if (!timeout & o.isVisible() && (intersect(o, lastplateL))) {
-				int midx = (o.getX() + o.getX() + o.getWidth()) / 2;
-				if (midx <= dummyL.getX() + dummyL.getWidth() && midx >= dummyL.getX()) {
-					moving.remove(o);
-					State state = new ControlledImageObject(o.getX(), o.getY() + 5);
-					o.setState(state);
-					control.add(o);
-					controlL.add(o);
-					saveStateL();
-					// ImageObject obj = (ImageObject) lastplateL;
-					// obj = (ImageObject) controlL.get(controlL.size()-2);
-					String color1 = o.getColor();
-					String color2 = ((ImageObject) lastplateL).getColor();
-					if (controlL.size() > 3) {
-						String color3 = ((ImageObject) controlL.get(controlL.size() - 3)).getColor();
-
-						if (color1.equals(color2) && color2.equals(color3)) {
-							this.notifyAllObserver(1);
-						}
-					}
-
-					/*
-					 * if(color1.equals(color2)) { System.out.println(true); countL++; }else {
-					 * if(color3.equals(color2)) { countL=1; }else { countL=0; } }
-					 * 
-					 * if (countL == 2 ) { this.notifyAllObserver(); }
-					 */
-
-				} else {
-					control.remove(lastplateL);
-					controlL.remove(lastplateL);
-					State state = new MovingImageObject();
-					((ImageObject) lastplateL).setState(state);
-					moving.add(lastplateL);
-					caretaker.removeL();
-					this.setCurrentMementoL(this.getCurrentMementoL() - 1);
-				}
-			}
-			if (!timeout & o.isVisible() && (intersect(o, lastplateR))) {
-				// clown caught a plate here on the right side
-				int midx = (o.getX() + o.getX() + o.getWidth()) / 2;
-				if (midx <= dummyR.getX() + dummyR.getWidth() && midx >= dummyR.getX()) {
-					moving.remove(o);
-					State state = new ControlledImageObject(o.getX(), o.getY() + 5);
-					((ImageObject) o).setState(state);
-					control.add(o);
-					controlR.add(o);
-					saveStateR();
-					String color1 = o.getColor();
-					String color2 = ((ImageObject) lastplateR).getColor();
-					if (controlR.size() > 3) {
-						String color3 = ((ImageObject) controlR.get(controlR.size() - 3)).getColor();
-						if (color1.equals(color2) && color2.equals(color3)) {
-							this.notifyAllObserver(2);
-						}
-					}
-				} else {
-					control.remove(lastplateR);
-					controlR.remove(lastplateR);
-					State state = new MovingImageObject();
-					((ImageObject) lastplateR).setState(state);
-					moving.add(lastplateR);
-					caretaker.removeR();
-					this.setCurrentMementoR(this.getCurrentMementoR() - 1);
-				}
-				/*
-				 * if(color1.equals(color2)) { countR++; }else { if(color3.equals(color2)) {
-				 * countR=1; }else { countR=0; } } if (countR ==2) { this.notifyAllObserver(); }
-				 */
-
-				// this.notifyAllObserver();
-			}
-
-		}
-		// itrator
-
-		Iterator_concrete itr2;
-		itr2 = new Iterator_concrete(controlL);
-		for (Iterator iter = itr2.getIterator(0); iter.hasNext();) {
-			GameObject o = (GameObject) iter.next();
-			o.setX((int) Math.min(o.getX(), scrwidth-310));
-		}
-		Iterator_concrete itr3;
-		itr3 = new Iterator_concrete(controlR);
-		for (Iterator iter = itr3.getIterator(0); iter.hasNext();) {
-			GameObject o = (GameObject) iter.next();
-			o.setX(Math.max(o.getX(), 157));
-		}
-
-		return !timeout;
-
+		logic.pooling();
+		logic.iterate();
+		return logic.refreshLogic();
 	}
 
-	// al taba2 b3dha l taba2 ele fo2
-	// m7taga design pattern ?
-	private boolean intersect(GameObject o1, GameObject o2) {
-		int midx = (o1.getX() + o1.getX() + o1.getWidth()) / 2;
-		// System.out.println(o1.getX());
-		// System.out.println(o1.getWidth());
-		/* && o1.getY() + o1.getHeight() <= o2.getY()+2 */
-		if (o1.getY() + o1.getHeight() == o2.getY() && (midx <= o2.getX() + o2.getWidth() && midx >= o2.getX())) {
-			// System.out.println("true");
-			return true;
-		}
-
-		return false;
-	}
 
 	@Override
 	public String getStatus() {
@@ -378,12 +229,6 @@ public class Circus implements World {
 		return this.score;
 	}
 
-	public void notifyAllObserver(int num) {
-		for (Observer observer : observers) {
-			observer.update(num);
-		}
-	}
-
 	public void setTime(int sTime) {
 		MAX_TIME = sTime;
 	}
@@ -392,22 +237,6 @@ public class Circus implements World {
 		this.score = score;
 	}
 
-	private void saveStateL() {
-		// TODO Auto-generated method stub
-		originator.set((ArrayList<GameObject>) controlL.clone());
-		caretaker.addMementoL(originator.storeInMemento());
-		currentMementoL++;
-	}
 
-	private void saveStateR() {
-		// TODO Auto-generated method stub
-		originator.set((ArrayList<GameObject>) controlR.clone());
-		caretaker.addMementoR(originator.storeInMemento());
-		currentMementoR++;
-	}
-	
-	public ArrayList<GameObject> getControl() {
-		return control;
-		
-	}
+
 }
---
+++ src/world_class/Facad.java
@@ -0,0 +1,193 @@
+package world_class;
+
+import eg.edu.alexu.csd.oop.game.GameObject;
+import eg.edu.alexu.csd.oop.game.World;
+import iterator.Iterator;
+import iterator.Iterator_concrete;
+import momento.Caretaker;
+import momento.Originator;
+import observer.*;
+import to_come.*;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Facad {
+    private Observer t;
+    private Observer s;
+    private Observer sc;
+    private Observer mom;
+    private Circus game;
+    private static Caretaker caretaker;
+    private static Originator originator;
+    private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+    public Facad(Circus game){
+        //initializing the observers
+        t = new Time(game);
+        s = new Sound(game);
+        sc = new Score(game);
+        mom = new plateServer(game);
+        this.game=game;
+        caretaker = game.getCaretaker();
+        originator = game.getOriginator();
+        saveStateL();
+        saveStateR();
+    }
+
+    public boolean refreshLogic(){
+        GameObject lastplateL = game.getControlL().get(game.getControlL().size() - 1);
+        GameObject lastplateR = game.getControlR().get(game.getControlR().size() - 1);
+        Iterator_concrete xx;
+        xx = new Iterator_concrete(game.getMoving()); // handle moving plates here
+        boolean timeout = System.currentTimeMillis() - game.getStartTime() > game.getMaxTime(); // time end and game over
+        for (Iterator iter = xx.getIterator(0); iter.hasNext();) {
+            ImageObject o = (ImageObject) iter.next();
+            o.setY((o.getY() + 1));
+            if (o.getY() + 100 >= game.getHeight()) {
+                // reuse the plate in another position
+                o.setY(-1 * (int) (Math.random() * game.getHeight()));
+                o.setX((int) (Math.random() * game.getWidth()));
+
+            }
+            // o.setX(o.getX() + (Math.random() > 0.5 ? 2 : -2));
+            // al taba2 b3dha l taba2 ele fo2
+            if (!timeout & o.isVisible() && (intersect(o, lastplateL))) {
+                int midx = (o.getX() + o.getX() + o.getWidth()) / 2;
+                if (midx <= game.getDummyL().getX() + game.getDummyL().getWidth() && midx >= game.getDummyL().getX()) {
+                    game.getMoving().remove(o);
+                    State state = new ControlledImageObject(o.getX(), o.getY() + 5);
+                    o.setState(state);
+                    game.getControl().add(o);
+                    game.getControlL().add(o);
+                    saveStateL();
+                    // ImageObject obj = (ImageObject) lastplateL;
+                    // obj = (ImageObject) controlL.get(controlL.size()-2);
+                    String color1 = o.getColor();
+                    String color2 = ((ImageObject) lastplateL).getColor();
+                    if (game.getControlL().size() > 3) {
+                        String color3 = ((ImageObject) game.getControlL().get(game.getControlL().size() - 3)).getColor();
+
+                        if (color1.equals(color2) && color2.equals(color3)) {
+                            notifyAllObserver(1);
+                        }
+                    }
+
+                    /*
+                     * if(color1.equals(color2)) { System.out.println(true); countL++; }else {
+                     * if(color3.equals(color2)) { countL=1; }else { countL=0; } }
+                     *
+                     * if (countL == 2 ) { this.notifyAllObserver(); }
+                     */
+
+                } else {
+                    game.getControl().remove(lastplateL);
+                    game.getControlL().remove(lastplateL);
+                    State state = new MovingImageObject();
+                    ((ImageObject) lastplateL).setState(state);
+                    game.getMoving().add(lastplateL);
+                    caretaker.removeL();
+                    game.setCurrentMementoL(game.getCurrentMementoL() - 1);
+                }
+            }
+            if (!timeout & o.isVisible() && (intersect(o, lastplateR))) {
+                // clown caught a plate here on the right side
+                int midx = (o.getX() + o.getX() + o.getWidth()) / 2;
+                if (midx <= game.getDummyR().getX() + game.getDummyR().getWidth() && midx >= game.getDummyR().getX()) {
+                    game.getMoving().remove(o);
+                    State state = new ControlledImageObject(o.getX(), o.getY() + 5);
+                    ((ImageObject) o).setState(state);
+                    game.getControl().add(o);
+                    game.getControlR().add(o);
+                    saveStateR();
+                    String color1 = o.getColor();
+                    String color2 = ((ImageObject) lastplateR).getColor();
+                    if (game.getControlR().size() > 3) {
+                        String color3 = ((ImageObject) game.getControlR().get(game.getControlR().size() - 3)).getColor();
+                        if (color1.equals(color2) && color2.equals(color3)) {
+                            notifyAllObserver(2);
+                        }
+                    }
+                } else {
+                    game.getControl().remove(lastplateR);
+                    game.getControlR().remove(lastplateR);
+                    State state = new MovingImageObject();
+                    ((ImageObject) lastplateR).setState(state);
+                    game.getMoving().add(lastplateR);
+                    caretaker.removeR();
+                    game.setCurrentMementoR(game.getCurrentMementoR() - 1);
+                }
+                /*
+                 * if(color1.equals(color2)) { countR++; }else { if(color3.equals(color2)) {
+                 * countR=1; }else { countR=0; } } if (countR ==2) { this.notifyAllObserver(); }
+                 */
+
+                // this.notifyAllObserver();
+            }
+
+        }
+
+        // itrator
+        return !timeout;
+    }
+
+    public void iterate(){
+
+        Iterator_concrete itr2;
+        itr2 = new Iterator_concrete(game.getControlL());
+        for (Iterator iter = itr2.getIterator(0); iter.hasNext();) {
+            GameObject o = (GameObject) iter.next();
+            o.setX((int) Math.min(o.getX(), screenSize.getWidth()));
+        }
+        Iterator_concrete itr3;
+        itr3 = new Iterator_concrete(game.getControlR());
+        for (Iterator iter = itr3.getIterator(0); iter.hasNext();) {
+            GameObject o = (GameObject) iter.next();
+            o.setX(Math.max(o.getX(), 157));
+        }
+    }
+    public void pooling(){
+        MovingPool mpl = MovingPool.getInstance();
+        if(mpl.hasElement()) {
+            State state = new MovingImageObject();
+            GameObject obj = mpl.useObj();
+            ((ImageObject)obj).setState(state);
+            obj.setX((int) (Math.random() * screenSize.width));
+            obj.setY((int) (Math.random() * screenSize.height));
+            game.getMoving().add(obj);
+        }
+    }
+
+    private boolean intersect(GameObject o1, GameObject o2) {
+        int midx = (o1.getX() + o1.getX() + o1.getWidth()) / 2;
+        // System.out.println(o1.getX());
+        // System.out.println(o1.getWidth());
+        /* && o1.getY() + o1.getHeight() <= o2.getY()+2 */
+        if (o1.getY() + o1.getHeight() == o2.getY() && (midx <= o2.getX() + o2.getWidth() && midx >= o2.getX())) {
+            // System.out.println("true");
+            return true;
+        }
+
+        return false;
+    }
+
+    private void saveStateL() {
+        originator.set((ArrayList<GameObject>) game.getControlL().clone());
+        caretaker.addMementoL(originator.storeInMemento());
+        game.setCurrentMementoL(game.getCurrentMementoL()+1);
+    }
+
+    private void saveStateR() {
+        originator.set((ArrayList<GameObject>) game.getControlR().clone());
+        caretaker.addMementoR(originator.storeInMemento());
+        game.setCurrentMementoR(game.getCurrentMementoR()+1);
+    }
+
+    private void notifyAllObserver(int num) {
+        for (Observer observer : game.getObservers()) {
+            observer.update(num);
+        }
+    }
+
+
+}
